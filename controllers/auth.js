c69cf0ad5006bbdfabcd565dc33ed5b260ee7513
olistaTD <107848031+olistaTD@users.noreply.github.com>
2023-05-15 11:06:42 +0200

    - Verificacion de correo, pass y estado en Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,50 @@
 
 const { response } = require('express');
+const bcryptjs = require('bcryptjs');
 
-const login = (req, res = response) => {
+const Usuario = require('../models/usuario');
 
-    res.json( {
-        msg: 'Login ok'
-    } )
+const login = async(req, res = response) => {
+
+    const { correo, password } =  req.body;
+
+    try {
+        const usuario = await Usuario.findOne( { correo } );
+
+        // Si email existe
+        if ( !usuario ) {
+            return res.status(400).json( {
+                msg: 'Usuario / Password no son correctos - passord: incorrecta'
+            } );
+        }
+
+        // Si usuario actuivo en BD
+        if ( !usuario.estado ) {
+            return res.status(400).json( {
+                msg: 'Usuario / Password no son correctos - estado: false'
+            } );
+        }
+
+        // Contraseña correcta
+        const validPassword = bcryptjs.compareSync( password, usuario.password );
+        if ( !validPassword ) {
+            return res.status(400).json( {
+                msg: 'Usuario / Password no son correctos - password'
+            } );
+        }
+
+        // Generar JWT
+
+        res.json( {
+            msg: 'Login ok'
+        } );
+
+    } catch (error) {
+        console.log( error );
+        return res.status(500).json( {
+            msg: 'Hable con el administrador'
+        } )
+    }
 }
 
 module.exports = {
