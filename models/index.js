fdd3630049c859b11d9fab346e3a211a89fdeb39
olistaTD <107848031+olistaTD@users.noreply.github.com>
2023-05-23 11:05:17 +0200

    - Añadido modelo Categoria

---
+++ models/index.js
@@ -0,0 +1,12 @@
+
+const Categorta = require('./categoria');
+const Role = require('./role');
+const Server = require('./server');
+const Usuario = require('./usuario');
+
+module.exports = {
+    Categorta,
+    Role,
+    Server,
+    Usuario
+}
