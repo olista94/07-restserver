ffaa30c62d9bc511823139b07e485df8daed5c37
olistaTD <107848031+olistaTD@users.noreply.github.com>
2023-05-25 14:44:58 +0200

    - Añadido subida de archivo

--- models/server.js
+++ models/server.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
 const { dbConnection } = require('../database/config');
 
 class Server {
@@ -15,6 +17,7 @@ class Server {
             buscar:      '/api/buscar',
             categorias:  '/api/categorias',
             productos:   '/api/productos',
+            uploads:     '/api/uploads',
             usuarios:    '/api/usuarios'
         }
 
@@ -43,6 +46,12 @@ class Server {
         
         // Directorio poublico
         this.app.use( express.static('public') );
+
+        // Fileupload- Carga de archivos
+        this.app.use( fileUpload ( {
+            useTempFiles : true,
+            tempFileDir : '/tmp/'
+        } ));
     }
 
     routes() {
@@ -51,6 +60,7 @@ class Server {
         this.app.use( this.paths.buscar, require('../routes/buscar') );
         this.app.use( this.paths.categorias, require('../routes/categorias') );
         this.app.use( this.paths.productos, require('../routes/productos') );
+        this.app.use( this.paths.uploads, require('../routes/uploads') );
         this.app.use( this.paths.usuarios, require('../routes/user') );
     }
 
