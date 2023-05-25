5116c953a791f8a2d70cc2ed9d702f5e6a73ef25
olistaTD <107848031+olistaTD@users.noreply.github.com>
2023-05-25 11:09:07 +0200

    - Añadida busqueda categoria, producto y usuario

--- models/server.js
+++ models/server.js
@@ -12,7 +12,8 @@ class Server {
 
         this.paths = {
             auth:        '/api/auth',
-            categorias: '/api/categorias',
+            buscar:      '/api/buscar',
+            categorias:  '/api/categorias',
             productos:   '/api/productos',
             usuarios:    '/api/usuarios'
         }
@@ -47,6 +48,7 @@ class Server {
     routes() {
 
         this.app.use( this.paths.auth, require('../routes/auth') );
+        this.app.use( this.paths.buscar, require('../routes/buscar') );
         this.app.use( this.paths.categorias, require('../routes/categorias') );
         this.app.use( this.paths.productos, require('../routes/productos') );
         this.app.use( this.paths.usuarios, require('../routes/user') );
